1d525a91051a003ca471a9736efdaa265b3dd9e3
shon_nuri <nadyrkhans@gmail.com>
2024-03-18 15:06:14 +0500

    home and login pages created

---
+++ js/print.js
@@ -0,0 +1,6 @@
+document.getElementById('submitButton').addEventListener('click', function() {
+    const inputValue = document.getElementById('inputField1').value;
+    const inputValue1 = document.getElementById('inputField2').value;
+    console.log('Input value:', inputValue);
+    console.log(inputValue1);
+  });
